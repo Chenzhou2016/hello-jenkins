357defc51a7864b8879d0c5ce12ce24e1c1465aa
Chenzhou2016 <zhou.chen@hpe.com>
2017-05-19 12:44:55 +0000

    change greeting message

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
- 
+
 var app = express();
- 
+
 app.get('/', function (req, res) {
-  res.send('hello jenkins 2016');
+  res.send('Greeting from Jenkins - Hello, Helen!');
 });
- 
+
 app.listen(process.env.PORT || 5000);
- 
+
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../app.js');
- 
+
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
     request(app).get('/').expect('hello jenkins 2016', done);
